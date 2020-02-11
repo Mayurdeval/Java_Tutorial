b22626d92f388f3716e9bb153ab75caac4e87750
Mayurdeval <60640679+Mayurdeval@users.noreply.github.com>
2020-02-11 11:18:52 +0530

    Update PrimeNumbers.java

--- Exam/PrimeNumbers.java
+++ Exam/PrimeNumbers.java
@@ -3,7 +3,7 @@ package Exam;
 public class PrimeNumbers {
 
 public static void main(String[] args) {
-    for(int i=3;i<=100;i++){
+    for(int i=2;i<=100;i++){
         int state = 0;
         for(int j=2;j<i;j++){
             if(i%j == 0)
